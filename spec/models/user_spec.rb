212adcf7aad4f8367df3b10ddbaa111d1f74a40e
Sam Thoburn <samuel.j.thoburn@outlook.com>
2021-11-16 11:24:46 +0000

    feature(user): Updated user model to include unique username and encrypted password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "must have a username and password" do
-    result = User.create(username: "test")
-    expect(result).to_not be_valid
+  context "must have username and password" do
+    it "is valid with a username and password" do
+      result = User.create(username: "test", password_digest: "test" )
+      expect(result).to be_valid
+    end
+
+    it "is invalid without a username" do
+      result = User.create(password_digest: "test" )
+      expect(result).to_not be_valid
+    end
+
+    it "is invalid without a password" do
+      result = User.create(username: "test" )
+      expect(result).to_not be_valid
+    end
   end
 end
