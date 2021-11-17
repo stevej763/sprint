1e0710a2ba52440164ac32bd264739a97c18804d
Steve Jones <steve.jones763@gmail.com>
2021-11-17 16:28:07 +0000

    feature(challenge): updated challenge schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_144119) do
+ActiveRecord::Schema.define(version: 2021_11_17_154738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,11 +45,9 @@ ActiveRecord::Schema.define(version: 2021_11_17_144119) do
 
   create_table "challenges", force: :cascade do |t|
     t.string "title"
-    t.string "icon_link"
     t.string "description"
     t.string "difficulty"
     t.integer "distance"
-    t.string "badge_link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
