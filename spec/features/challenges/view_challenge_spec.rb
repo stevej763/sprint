1494f7f9e6eaf4da1a2b3b8b069f5a807d665312
Steve Jones <steve.jones763@gmail.com>
2021-11-17 12:06:09 +0000

    feature(challenge): Added message when challenge ID does not exist

--- spec/features/challenges/view_challenge_spec.rb
+++ spec/features/challenges/view_challenge_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 RSpec.feature "Viewing challenges", :type => :feature do
   fixtures :users, :challenge
-  scenario "User can click sign up and create their account" do
-    sign_in_existing_user
-    visit("/challenge/1")
-    expect(page).to have_content("Test challenge")
-    expect(page).to have_content("This is a challenge") 
-    expect(page).to have_content("Easy")
-    expect(page).to have_content("10")
+  context "User goes to a challenge page" do
+    scenario "User can go to a valid challenge page." do
+      sign_in_existing_user
+      visit("/challenge/1")
+      expect(page).to have_content("Test challenge")
+      expect(page).to have_content("This is a challenge") 
+      expect(page).to have_content("Easy")
+      expect(page).to have_content("10")
+      expect(page).to_not have_content("That challenge id does not exist")
 
+    end
+    scenario "User tries to go to invalid challenge page" do
+      sign_in_existing_user
+      visit("/challenge/1000")
+      expect(page).to have_content("That challenge id does not exist")
+
+    end
   end
+  
 end
