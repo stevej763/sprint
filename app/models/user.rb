3ab72599b02b59f61e8425b2369d49f674faabb2
khalid <khalidmiah@hotmail.co.uk>
2021-11-18 11:55:47 +0000

    feature(profile-image):added logic to upload an image

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: true
   has_secure_password
+  has_one_attached :profile_image
 end
