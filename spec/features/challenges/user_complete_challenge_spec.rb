f9bdb320223b4dd34e4a6f287123c0bd3391c70b
Sam Thoburn <samuel.j.thoburn@outlook.com>
2021-11-22 15:26:33 +0000

    feature(add-activity): Fixed feature test and CSS on add activity page

--- spec/features/challenges/user_complete_challenge_spec.rb
+++ spec/features/challenges/user_complete_challenge_spec.rb
@@ -4,9 +4,10 @@ RSpec.feature "Adding distance", :type => :feature do
   context "User inputs distance" do
     scenario "from add distance page" do
       sign_in_user_with_active_challenge
-      click_button "Add distance"
-      fill_in "distance", with: "10"
-      click_button "Add distance"
+      click_button "Add activity"
+      fill_in "activity_name", with: "My activity"
+      fill_in "activity_distance", with: "50"
+      click_button "Add activity"
       expect(page).to have_current_path('/completed-challenge/1')
       expect(page).to have_content 'You have completed:'
       expect(page).to have_content 'Test challenge'
