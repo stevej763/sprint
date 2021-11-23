fe67617f9ad1a02a52be86ccb056c19d68472952
Steve Jones <steve.jones763@gmail.com>
2021-11-23 17:08:00 +0000

    feature(checkpoints): Now colours in checkpoints as progress is made.

--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -24,4 +24,17 @@ RSpec.describe Challenge, type: :model do
       expect(result).to_not be_valid
     end
   end   
+
+  context "Checkpoint gets highlighted" do
+    it "has a unique name" do
+      Challenge.create(title: "test1")
+      result = Challenge.create(title: "test2")
+      expect(result).to be_valid
+    end
+    it "has a dublicate name" do
+      Challenge.create(title: "test1")
+      result = Challenge.create(title: "test1")
+      expect(result).to_not be_valid
+    end
+  end   
 end
