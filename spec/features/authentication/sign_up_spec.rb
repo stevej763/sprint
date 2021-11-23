1e036123395f43f049a7059be5c443786e246ceb
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-11-23 10:22:42 +0000

    style(flash message): refactored scenario of feature test to use a helper to reduce lines of code

--- spec/features/authentication/sign_up_spec.rb
+++ spec/features/authentication/sign_up_spec.rb
@@ -35,17 +35,7 @@ RSpec.feature "User sign up", :type => :feature do
   end
 
   scenario "User cannot signup without a unique username" do
-    visit "/"
-    click_button "Sign up"
-
-    expect(page).to have_current_path("/sign-up")
-    fill_in "user_username", with: "test"
-    fill_in "user_password", with: "password"
-    click_button "Sign up"
-
-    expect(page).to have_current_path("/homepage")
-    expect(find_by_id("current_user").text).to eq("test")
-    expect(page).to have_content("You have signed up.")
+    sign_up_user
 
     click_button "Sign out"
     expect(page).to have_current_path("/")
@@ -59,5 +49,4 @@ RSpec.feature "User sign up", :type => :feature do
     expect(page).to have_current_path("/")
     expect(page).to have_content("Username already in use. Please try again.")
   end
-
 end
