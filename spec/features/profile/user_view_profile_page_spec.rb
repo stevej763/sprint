82078e76dbef1ddd33cc582be663be4e3b0de0eb
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-11-23 14:52:16 +0000

    feature(profile-page): completed feature test

--- spec/features/profile/user_view_profile_page_spec.rb
+++ spec/features/profile/user_view_profile_page_spec.rb
@@ -11,10 +11,15 @@ RSpec.feature "View badges on profile page:", :type => :feature do
     end
     scenario "to see completed challenge badge" do
       sign_in_existing_user
-
-      # visit("/profile")
-      # expect(page).to have_current_path('/profile')
-      # expect(page).to have_content('Hunt the Horcruxes')
+      sign_up_for_challenge
+      click_button "Add activity"
+      fill_in "activity_name", with: "My activity"
+      fill_in "activity_distance", with: "50"
+      click_button "Add activity"
+      expect(page).to have_current_path('/completed-challenge/1')
+      click_button "View your badges"
+      expect(page).to have_current_path('/profile')
+      expect(page).to have_content('Test challenge')
     end
   end
 end
