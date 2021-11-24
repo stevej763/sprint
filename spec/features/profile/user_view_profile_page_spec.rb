6dfd47b7949b554be790447be7a239c1fd03c794
Steve Jones <steve.jones763@gmail.com>
2021-11-24 10:18:49 +0000

    fixed tests.

--- spec/features/profile/user_view_profile_page_spec.rb
+++ spec/features/profile/user_view_profile_page_spec.rb
@@ -13,7 +13,15 @@ RSpec.feature "View badges on profile page:", :type => :feature do
       visit("/profile")
       expect(page).to have_current_path('/profile')
       expect(page).to have_content('Looks a little empty here, complete a challenge to show off your badges')
-      expect(page).to have_button("Pick a challenge")
+      expect(page).to have_button("Find a challenge")
+    end
+    scenario "User has started a challenge" do
+      sign_in_existing_user
+      sign_up_for_challenge
+      visit("/profile")
+      expect(page).to have_current_path('/profile')
+      expect(page).to have_content('Looks a little empty here, complete a challenge to show off your badges')
+      expect(page).to have_button("Go to active challenge")
     end
     scenario "to see completed challenge badge" do
       sign_in_existing_user
